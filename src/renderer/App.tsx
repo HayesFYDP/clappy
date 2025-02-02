fecff99baba36ae2fed1f4ad9e029e914886575e
Hannah Guo <hannah.guo@makenotion.com>
2025-02-01 23:56:49 -0500

    initial styled hover UI (#2)

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,4 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
 import smiskiIcon from '../../assets/smiski.png';
 import './App.css';
 
@@ -7,10 +7,25 @@ function Hello() {
     <div>
       <div id="popup">
         <div id="speech-bubble">Hi!</div>
-        <div id="settings">
-          <button type="button" id="settings-button">
-            ⚙️
-          </button>
+        <div id="hover-rows">
+          <div id="settings" className="hover-row">
+            <span>View settings</span>
+            <button type="button" id="settings-button">
+              ⚙️
+            </button>
+          </div>
+          <div id="history" className="hover-row">
+            <span>View history</span>
+            <button type="button" id="history-button">
+              🕰️
+            </button>
+          </div>
+          <div id="talk-to" className="hover-row">
+            <span>Talk to Clappy</span>
+            <button type="button" id="talk-to-button">
+              💬
+            </button>
+          </div>
         </div>
         <img src={smiskiIcon} alt="Character" />
       </div>
