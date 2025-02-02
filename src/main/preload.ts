73290965c5cd47f1959521ec0b6799e0a20d731f
unknown <elena.pan.ep@gmail.com>
2025-02-01 20:06:42 -0500

    feat: add keyboard shortcut to popup clappy

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'open-popup' | 'close-popup' | 'set-ignore-mouse-events' | 'add-mouse-event-listeners';
+export type Channels = 'open-popup' | 'close-popup' | 'toggle-popup' | 'set-ignore-mouse-events' | 'add-mouse-event-listeners';
 
 const electronHandler = {
   ipcRenderer: {
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,33 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ClappyExpression } from '../main/types';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
+
+function openPopup(expression: ClappyExpression, text: string | null): void {
+  const speechBubble = document.getElementById('speech-bubble') as HTMLElement;
+  if (text !== null && text !== undefined && text !== '') { 
+    speechBubble.style.display = 'block';
+    speechBubble.textContent = text as string;
+  } else {
+    speechBubble.style.display = 'none';
+  }
+
+  // TODO: change Clappy image based on expression
+  const popup = document.getElementById('popup') as HTMLElement;
+  popup.style.display = 'block';
+  popup.classList.add('visible');
+}
+
+function closePopup(): void {
+  const popup = document.getElementById('popup') as HTMLElement;
+  popup.classList.remove('visible');
+  setTimeout(() => {
+    popup.style.display = 'none';
+  }, 500); // Wait for the transition before hiding
+}
 
 window.electron.ipcRenderer.on('add-mouse-event-listeners', () => {
   const popup = document.getElementById('popup');
@@ -24,19 +48,19 @@ window.electron.ipcRenderer.on('add-mouse-event-listeners', () => {
 });
 
 // Listen for the open-popup message from the main process
-window.electron.ipcRenderer.on('open-popup', (productive) => {
-  const speechBubble = document.getElementById('speech-bubble') as HTMLElement;
-  speechBubble.textContent = productive ? 'Good job!' : 'GET BACK TO WORK';
-
-  const popup = document.getElementById('popup') as HTMLElement;
-  popup.style.display = 'block';
-  popup.classList.add('visible');
+window.electron.ipcRenderer.on('open-popup', (expression, text) => {
+  openPopup(expression as ClappyExpression, text as string | null);
 });
 
 window.electron.ipcRenderer.on('close-popup', () => {
+  closePopup();
+});
+
+window.electron.ipcRenderer.on('toggle-popup', () => {
   const popup = document.getElementById('popup') as HTMLElement;
-  popup.classList.remove('visible');
-  setTimeout(() => {
-    popup.style.display = 'none';
-  }, 500); // Wait for the transition before hiding
+  if (popup.classList.contains('visible')) {
+    closePopup();
+  } else {
+    openPopup(ClappyExpression.Happy, 'GET BACK TO WORK');
+  }
 });
