265807865c9dc1f60880ade096ef3dc784409c49
Sunny Zuo <me@sunnyzuo.com>
2025-03-23 15:44:20 -0400

    fix: save intervention history properly

--- src/main/main.ts
+++ src/main/main.ts
@@ -10,17 +10,17 @@ if (process.env.NODE_ENV === 'production') {
   sourceMapSupport.install();
 }
 
-const IS_DEVELOPMENT = true; // TODO: Set this to false when deploying or take as an arg
+const IS_DEVELOPMENT = false; // TODO: Set this to false when deploying or take as an arg
 // if set to true, will randomly select interventions when IS_DEVELOPMENT is true; otherwise, no interventions will be taken
-const DEVELOPMENT_INTERVENTION_ENABLED = true;
+const DEVELOPMENT_INTERVENTION_ENABLED = false;
 const MEMORY_ENABLED = false; // whether or not to use memory - in theory we get better reasoning but it uses a lot more tokens
 
 const ENABLED_INTERVENTIONS: Interventions[] = [
-  // Interventions.POPUP_CLAPPY,
-  Interventions.MINIMIZE_WINDOW,
-  Interventions.SHAKE_WINDOW,
-  Interventions.FOCUS_WINDOW,
+  Interventions.POPUP_CLAPPY,
   // Interventions.SPEAK_CLAPPY,
+  Interventions.SHAKE_WINDOW,
+  Interventions.MINIMIZE_WINDOW,
+  Interventions.FOCUS_WINDOW,
 ];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
