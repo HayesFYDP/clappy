5acfea3e0865a4a2ee71baac36d693e2c7a522e5
unknown <elena.pan.ep@gmail.com>
2025-03-23 17:22:59 -0400

    fix: clappy interaction manager clarification

--- src/main/clappyInteractionManager.ts
+++ src/main/clappyInteractionManager.ts
@@ -44,8 +44,6 @@ export default class ClappyInteractionManager {
     const choicePreferencesString = !isUserTaskSet
       ? `Your main objective is to determine what the user is currently trying to accomplish to record it in your memory using SET_TASK.
         You should use CLARIFY if you are unsure about the user's task or if what the user provided is too vague for you to be a effective assistant.
-        For example, if the user says that they want to get work done, you should use CLARIFY and ask them what kind of work they want to get done.
-        For example, If the user says that they want to complete a task, you should use CLARIFY to obtain more details about the task so that you can accurately determine if what the user is viewing on their screen is related to their task.
         You want to get enough context so that when you observe the user's screen, you can confidently determine if it is related to their task.`
       : `You should use SET_TASK if it is clear that the user wishes to change the task they are working on.
         Otherwise, your preference is to update your memory with UPDATE_MEMORY to better help the user in the future. For example, if the user tells you that they want to stop being distracted by a program or topic, you should UPDATE_MEMORY with that information.
@@ -162,7 +160,9 @@ export default class ClappyInteractionManager {
   }
 
   async startVoiceInteraction(): Promise<void> {
-    this.clappy.mainWindow?.webContents.send('open-popup-interact', ClappyExpression.OffersMicrophone, 'listening...');
+    setTimeout(() => {
+      this.clappy.mainWindow?.webContents.send('open-popup-interact', ClappyExpression.Thinking, 'Listening...');
+    }, 1000);
 
     try {
       console.log('[VOICE] Starting voice interaction');
